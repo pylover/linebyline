e1d22dc04252939b69781bf4e4fa0fcd194070e7
pylover <vahid.mardani@gmail.com>
2022-04-23 21:09:41 +0430

    Func & variable parser

--- tests/test-parser.hs
+++ tests/test-parser.hs
@@ -40,3 +40,6 @@ test_parse = do
 
   assertEqual (Group [Var "1", Var "2"]) 
     $ parse "$1 $2"
+
+  assertEqual (Func "join" [Var "1", Var "2", Var "bar", Var "3"]) 
+    $ parse "join $1 $2 $bar $3"
