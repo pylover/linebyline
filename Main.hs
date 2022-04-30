4b896fa054cddc9860e47ee2cc01b2c6d37cf31a
pylover <vahid.mardani@gmail.com>
2022-05-01 03:50:25 +0430

    Using haskeline hackage to read the standard input line by line

--- Main.hs
+++ Main.hs
@@ -1,6 +1,9 @@
 module Main where
 
 
+import System.IO
+import System.Console.Haskeline
+
 import HackLine.CLI
 
 
@@ -9,4 +12,12 @@ main = parseArgs >>= greet
 
 
 greet :: Args -> IO ()
-greet (Args script) = putStrLn script
+greet _ = runInputT defaultSettings loop
+
+
+loop :: InputT IO ()
+loop = do
+  minput <- getInputLine ""
+  case minput of
+    Nothing -> return ()
+    Just input -> outputStrLn input >> loop
